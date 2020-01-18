bc028de60b30d7c6e631025730ec4c0a4d7d8e9c
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2020-01-18 18:39:08 +0900

    Updated weather forecast announcement.

--- tenki.py
+++ tenki.py
@@ -23,59 +23,102 @@ if __name__ == '__main__':
         nav_id = 0
         p = re.compile(r"<[^>]*?>")
         flg_startweather = 0
-        flg_startweather = 0
+        flg_firstweather = 0
+        msg_pref = ""
         msg_place = ""
+        msg_time = ""
         msg_weather = ""
+        msg_info = ""
+        msg_city = ""
+        msg_min = ""
+        msg_max = ""
         #print("Start parsing")
         for line in r.text.splitlines():
             if line.find("</h1>") > -1:
                 res = re.sub(r"<[^>]*?>", "", line.strip()).split(" ")
                 msg_pref = res[1]
-                #print(msg_pref)
-            elif flg_startweather == 0 and line.find("<div style=\"float: left\">") > -1:
+            elif line.find("<div style=\"float: left\">") > -1:
+                if flg_startweather == 1:
+                    # store already parsed data
+                    #print("store weather")
+                    weather_info[msg_place].append((msg_time, msg_weather, msg_info, msg_city, msg_min, msg_max))
+                    msg_time = ""
+                    msg_weather = ""
+                    msg_info = ""
+                    msg_city = ""
+                    msg_min = ""
+                    msg_max = ""
                 msg_place = re.sub(r"<[^>]*?>", "", line.strip())
-                #print("place: '{}'".format(msg_place))
                 weather_info[msg_place] = []
-            elif flg_startweather == 0 and line.find("<th class=\"weather\">") > -1:
+            elif line.find("<th class=\"weather\">") > -1:
                 #print("start weather")
+                if flg_startweather == 1:
+                    # store already parsed data
+                    #print("store weather")
+                    weather_info[msg_place].append((msg_time, msg_weather, msg_info, msg_city, msg_min, msg_max))
+                    msg_time = ""
+                    msg_weather = ""
+                    msg_info = ""
+                    msg_city = ""
+                    msg_min = ""
+                    msg_max = ""
                 flg_startweather = 1
-            elif flg_startweather == 1 and line.find("</th>") > -1:
-                #print("end weather")
+                flg_firstweather = 1
+            elif line.find("class=\"fortemplate\"") > -1:
+                print("end weather")
+                if flg_startweather == 1:
+                    # store already parsed data
+                    #print("store weather")
+                    weather_info[msg_place].append((msg_time, msg_weather, msg_info, msg_city, msg_min, msg_max))
+                    msg_time = ""
+                    msg_weather = ""
+                    msg_info = ""
+                    msg_city = ""
+                    msg_min = ""
+                    msg_max = ""
                 flg_startweather = 0
-            elif flg_startweather == 1 :#and line.find("</th>") > -1:
-                #print("weather1: '{}'".format(line.strip()))
-                #res = p.sub(" ", line.strip())
+            elif line.find("class=\"info\"") > -1:
+                tmp = line.strip().split("<br>")
+                msg_info = re.sub(r"<[^>]*?>", "", tmp[0])
+            elif line.find("class=\"city\"") > -1:
+                msg_city = re.sub(r"<[^>]*?>", "", line.strip())
+            elif line.find("class=\"max\"") > -1:
+                msg_max = re.sub(r"<[^>]*?>", "", line.strip())
+            elif line.find("class=\"min\"") > -1:
+                msg_min = re.sub(r"<[^>]*?>", "", line.strip())
+            elif flg_firstweather == 1:
                 res = line.strip()
                 msg_weather = res
                 tmp2 = res.split("\"")
                 tmp1 = res.split("<")
-                #print(tmp1)
-                #print(tmp2)
-                time = tmp1[0]
-                weather = tmp2[5]
-                #print("{}:{}".format(time, weather))
-                weather_info[msg_place].append((time, weather))
+                msg_time = tmp1[0]
+                msg_weather = tmp2[5]
+                flg_firstweather = 0
         #print(msg_place)
         #print(msg_weather)
         #print(weather_info)
 
         target_area = '東部'
-        #print("output file")
-        #filename = "testout.html"
-        #with open(filename, mode='w') as f:
-        #    wi = weather_info[target_area]
-        #    f.write("<html><head></head><body>")
-        #    f.write("<table>")
-        #    for item in wi:
-        #        f.write("<tr><td>'{}'</td><td>'{}'</td></tr>".format(item[0], item[1]))
-        #    f.write("</table>")
-        #    f.write("</body></html>")
         wi = weather_info[target_area]
         msg_forecast = ""
+        itemcnt = 0
         for item in wi:
             val = item[1].replace(u'後', u'のち')
             msg_forecast += item[0] + u'、' + val + u'。'
-        msg_speak = msg_pref + target_area + u'の天気予報。' + msg_forecast
+            val = item[2].replace(u'後', u'のち')
+            val = val.replace(u'海上　では', u'海上では')
+            val = val.replace(u'　から', u'から')
+            msg_forecast += val + u'。'
+            if item[3] != "":
+                msg_forecast += item[3] + u'の気温。'
+            if item[4] != "":
+                msg_forecast += u'最低、' + item[4] + u'。'
+            if item[5] != "":
+                msg_forecast += u'最高、' + item[5] + u'。'
+            if itemcnt >= 1:
+                break # upto two items are used
+            itemcnt += 1
+        msg_speak = msg_pref + target_area + u'の天気予報をお伝えします。' + msg_forecast + u'以上です。バイバイ。'
         #print(msg_speak)
         talk.speak_message(msg_speak)
 
